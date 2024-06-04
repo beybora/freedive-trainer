0ea9c18ef4727d22da95397f9933b95269703e86
borabey <bora.b.sarikaya@gmail.com>
2024-06-04 14:17:39 +0200

    add session provider to Providers function

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,18 +4,21 @@ import { AppWrapper } from "@/context";
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { SessionProvider } from "next-auth/react";
 
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ChakraProvider>
-      <AppWrapper>
-        <QueryClientProvider client={queryClient}>
-          <ReactQueryDevtools initialIsOpen={false} />
-          {children}
-        </QueryClientProvider>
-      </AppWrapper>
-    </ChakraProvider>
+    <SessionProvider>
+      <ChakraProvider>
+        <AppWrapper>
+          <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen={false} />
+            {children}
+          </QueryClientProvider>
+        </AppWrapper>
+      </ChakraProvider>
+    </SessionProvider>  
   );
 }
