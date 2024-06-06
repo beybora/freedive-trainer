6f6d8a56048e37c884049545bb917b1b81055583
borabey <bora.b.sarikaya@gmail.com>
2024-06-06 11:47:49 +0200

    create navbar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import React, { useContext, useState } from "react";
+import { ChevronDownIcon, BellIcon } from "@chakra-ui/icons";
+import { useSession, signOut } from "next-auth/react";
+import ProfileModal from "./ProfileModal";
+import Logo from "../assets/logo.png";
+
+import {
+  Box,
+  Button,
+  Avatar,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+} from "@chakra-ui/react";
+
+const NavBar = () => {
+  const { data: session } = useSession();
+  console.log("session", session);
+
+  const [isProfileModalOpen, setIsProfileModalOpen] = useState<boolean>(false);
+
+  const openProfileModal = () => {
+    setIsProfileModalOpen(true);
+  };
+
+  const closeProfileModal = () => {
+    setIsProfileModalOpen(false);
+  };
+
+  return (
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      bg="black"
+      width="100%"
+      padding="5px 10px 5px 10px"
+      borderColor={"darkgreen"}
+    >
+      <Box>
+        <img
+          src={Logo.src}
+          alt="Logo"
+          style={{ height: "50px", width: "auto" }}
+        />
+      </Box>
+      <Box display="flex" gap="10px">
+        {/* <Menu>
+          <MenuButton>
+            <BellIcon fontSize="2xl" m={1} />
+          </MenuButton>
+        </Menu> */}
+        <Menu>
+          <MenuButton
+            as={Button}
+            borderRadius={"14"}
+            bg="white"
+            rightIcon={<ChevronDownIcon />}
+          >
+            <Avatar
+              size="sm"
+              cursor="pointer"
+              name={session?.user?.name ?? "User"}
+            />
+          </MenuButton>
+          <MenuList>
+            <MenuItem onClick={openProfileModal}>My Profile</MenuItem>
+            <MenuDivider />
+            <MenuItem onClick={() => signOut()}>Logout</MenuItem>
+          </MenuList>
+        </Menu>
+      </Box>
+      {session && (
+        <ProfileModal isOpen={isProfileModalOpen} onClose={closeProfileModal} />
+      )}
+    </Box>
+  );
+};
+
+export default NavBar;
