7715711cb2d78757c7d622fd032b01b5a0bac8a6
borabey <bora.b.sarikaya@gmail.com>
2024-06-13 16:27:17 +0200

    create a basic multisteper for creating group training

--- src/components/GroupTrainingModal.tsx
+++ src/components/GroupTrainingModal.tsx
@@ -3,6 +3,7 @@ import {
   FormControl,
   FormErrorMessage,
   FormLabel,
+  Heading,
   Input,
   Modal,
   ModalBody,
@@ -13,6 +14,8 @@ import {
 } from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
+import { useEffect, useState } from "react";
+import { ArrowLeftIcon, ArrowRightIcon } from "@chakra-ui/icons";
 
 type Props = {
   isOpen: boolean;
@@ -21,17 +24,53 @@ type Props = {
 
 type FormValues = {
   place: string;
-  date: Date;
+  datetime: Date;
   number: number;
+  depth: number;
 };
 
 const onSubmit = (data: FormValues) => {
   console.log("data: ", data);
 };
 
+const steps = [
+  {
+    label: "Step 1",
+    content: "Step 1 content",
+  },
+  {
+    label: "Step 2",
+    content: "Step 2 content",
+  },
+  {
+    label: "Step 3",
+    content: "Step 3 content",
+  },
+];
+
 const GroupTrainingModal = ({ isOpen, onClose }: Props) => {
-  const { register, control, handleSubmit, formState } = useForm<FormValues>();
+  const { register, control, handleSubmit, formState, reset } = useForm<FormValues>();
   const { errors } = formState;
+  const [currentStep, setCurrentStep] = useState(0);
+
+  const next = () => {
+    if (currentStep < steps.length - 1) {
+      setCurrentStep(currentStep + 1);
+    }
+  };
+
+  const prev = () => {
+    if (currentStep > 0) {
+      setCurrentStep(currentStep - 1);
+    }
+  };
+
+  useEffect(() => {
+    if (!isOpen) {
+      setCurrentStep(0);
+      reset();
+    }
+  }, [isOpen]);
 
   return (
     <>
@@ -39,58 +78,99 @@ const GroupTrainingModal = ({ isOpen, onClose }: Props) => {
         <form onSubmit={handleSubmit(onSubmit)}>
           <ModalOverlay />
           <ModalContent padding="2rem">
-            <ModalHeader>Create a new Group Training</ModalHeader>
+            <ModalHeader>Group Training</ModalHeader>
             <ModalBody>
-              <FormControl isInvalid={!!errors.place}>
-                <FormLabel>Place</FormLabel>
-                <Input
-                  type="text"
-                  id="place"
-                  placeholder="Place"
-                  {...register("place", { required: "This field is required" })}
-                />
-                <FormErrorMessage>
-                  {errors.place && errors.place.message}
-                </FormErrorMessage>
-              </FormControl>
-              <FormControl isInvalid={!!errors.date}>
-                <FormLabel>Date</FormLabel>
-                <Input
-                  type="date"
-                  id="date"
-                  placeholder="Date"
-                  {...register("date", { required: "This field is required" })}
-                />
-                <FormErrorMessage>
-                  {errors.date && errors.date.message}
-                </FormErrorMessage>
-              </FormControl>
-              <FormControl isInvalid={!!errors.number}>
-                <FormLabel>Number of dives</FormLabel>
-                <Input
-                  type="number"
-                  id="number"
-                  placeholder="Number"
-                  {...register("number", {
-                    required: "This field is required",
-                  })}
-                />
-                <FormErrorMessage>
-                  {errors.number && errors.number.message}
-                </FormErrorMessage>
-              </FormControl>
+              {currentStep === 0 && (
+                <>
+                  <FormControl isInvalid={!!errors.place}>
+                    <FormLabel>Where do you want to dive?</FormLabel>
+                    <Input
+                      type="text"
+                      id="place"
+                      placeholder="Place"
+                      {...register("place", {
+                        required: "This field is required",
+                      })}
+                    />
+                    <FormErrorMessage>
+                      {errors.place && errors.place.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                  <FormControl isInvalid={!!errors.datetime}>
+                    <FormLabel>Which day and which time?</FormLabel>
+                    <Input
+                      type="datetime-local"
+                      id="datetime"
+                      placeholder="Select Date and Time"
+                      {...register("datetime", {
+                        required: "This field is required",
+                      })}
+                    />
+                    <FormErrorMessage>
+                      {errors.datetime && errors.datetime.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                </>
+              )}
+              {currentStep === 1 && (
+                <>
+                  <FormControl isInvalid={!!errors.number}>
+                    <FormLabel>How many dives do you want to do?</FormLabel>
+                    <Input
+                      type="number"
+                      id="number"
+                      placeholder="Number"
+                      {...register("number", {
+                        required: "This field is required",
+                      })}
+                    />
+                    <FormErrorMessage>
+                      {errors.number && errors.number.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                  <FormControl isInvalid={!!errors.number}>
+                    <FormLabel>What is your targeted depth?</FormLabel>
+                    <Input
+                      type="number"
+                      id="depth"
+                      placeholder="Targeted Depth in meters"
+                      {...register("depth", {
+                        required: "This field is required",
+                      })}
+                    />
+                    <FormErrorMessage>
+                      {errors.number && errors.number.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                </>
+              )}
+              {currentStep === 2 && <></>}
             </ModalBody>
             <ModalFooter>
-              <Button
-                colorScheme="blue"
-                mr={3}
-                onClick={handleSubmit(onSubmit)}
-              >
-                Save
-              </Button>
-              <Button colorScheme="red" onClick={onClose}>
-                Close
-              </Button>
+              {currentStep > 0 && (
+                <Button
+                  mr={3}
+                  onClick={prev}
+                  colorScheme="blackAlpha"
+                  leftIcon={<ArrowLeftIcon />}
+                />
+              )}
+
+              {currentStep === steps.length - 1 ? (
+                <Button
+                  colorScheme="blue"
+                  mr={3}
+                  onClick={handleSubmit(onSubmit)}
+                >
+                  Save
+                </Button>
+              ) : (
+                <Button
+                  colorScheme="blackAlpha"
+                  onClick={next}
+                  leftIcon={<ArrowRightIcon />}
+                />
+              )}
             </ModalFooter>
           </ModalContent>
         </form>
