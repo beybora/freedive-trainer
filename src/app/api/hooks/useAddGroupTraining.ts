0022402abf0f93dca6f58475a875c35b95d6c8a7
borabey <bora.b.sarikaya@gmail.com>
2024-06-16 14:56:26 +0200

    create hook to add group traing

---
+++ src/app/api/hooks/useAddGroupTraining.ts
@@ -0,0 +1,18 @@
+import { GroupTraining } from "@/constants/optionsAndTypes";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+const addGroupTraining = async (data: GroupTraining) => {
+  const response = await fetch("/api/group-training", {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+  return response.json();
+};
+
+export const useAddGroupTraining = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (data: GroupTraining) => addGroupTraining(data),
+    onSuccess: () => {},
+  });
+};
