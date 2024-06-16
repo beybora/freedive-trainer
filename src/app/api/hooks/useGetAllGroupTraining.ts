05f30ba0fdbad0fb2506b0228d6ef2134c80171a
borabey <bora.b.sarikaya@gmail.com>
2024-06-16 14:56:46 +0200

    create hook to fetch all group training entries

---
+++ src/app/api/hooks/useGetAllGroupTraining.ts
@@ -0,0 +1,17 @@
+import { GroupTraining } from "@/constants/optionsAndTypes";
+import { useQuery } from "@tanstack/react-query";
+import { getDivesQueryKey } from "./useDives";
+
+const getAllGroupTraining = async (): Promise<GroupTraining[]> => {
+  const response = await fetch("api/group-training");
+  return response.json();
+};
+
+export const getAllGroupTrainingQueryKey = () => ["get-all-group-training"];
+
+export const useGetAllGroupTraining = () => {
+  return useQuery({
+    queryFn: getAllGroupTraining,
+    queryKey: getDivesQueryKey(),
+  });
+};
