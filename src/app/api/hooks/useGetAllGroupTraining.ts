6a646b28f971596abcec8b63d77d429fa5b2bb6d
borabey <bora.b.sarikaya@gmail.com>
2024-06-28 09:54:35 +0200

    fix: correct query key for fetching all group training data

--- src/app/api/hooks/useGetAllGroupTraining.ts
+++ src/app/api/hooks/useGetAllGroupTraining.ts
@@ -3,15 +3,15 @@ import { useQuery } from "@tanstack/react-query";
 import { getDivesQueryKey } from "./useDives";
 
 const getAllGroupTraining = async (): Promise<GroupTraining[]> => {
-  const response = await fetch("api/group-training");
+  const response = await fetch("/api/group-training");
   return response.json();
 };
 
-export const getAllGroupTrainingQueryKey = () => ["get-all-group-training"];
+export const getAllGroupTrainingQueryKey = () => ["group-training"];
 
 export const useGetAllGroupTraining = () => {
   return useQuery({
     queryFn: getAllGroupTraining,
-    queryKey: getDivesQueryKey(),
+    queryKey: getAllGroupTrainingQueryKey(),
   });
 };
