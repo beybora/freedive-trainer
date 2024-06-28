88fef4935f1262fbb3093f8bfcc65becb2f2234a
borabey <bora.b.sarikaya@gmail.com>
2024-06-28 09:44:58 +0200

    add grouptraining and session array of object Ids to schema, add virutal property "id"

--- models/user.ts
+++ models/user.ts
@@ -4,6 +4,8 @@ interface IUser extends Document {
   name: string;
   email: string;
   password: string;
+  groupTrainings: mongoose.Types.ObjectId[];
+  sessions: mongoose.Types.ObjectId[];
 }
 
 const userSchema = new Schema<IUser>(
@@ -11,9 +13,21 @@ const userSchema = new Schema<IUser>(
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
+    groupTrainings: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "GroupTraining" },
+    ],
+    sessions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Session" }],
   },
   { timestamps: true }
 );
+
+userSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+userSchema.set("toJSON", {
+  virtuals: true,
+});
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
 
