00af12ddf26d68b1614ad07cacb3fecdf436d73e
borabey <bora.b.sarikaya@gmail.com>
2024-06-28 09:57:11 +0200

    correct query key

--- src/app/api/hooks/useAddGroupTraining.ts
+++ src/app/api/hooks/useAddGroupTraining.ts
@@ -1,5 +1,6 @@
 import { GroupTraining } from "@/constants/optionsAndTypes";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getAllGroupTrainingQueryKey } from "./useGetAllGroupTraining";
 
 const addGroupTraining = async (data: GroupTraining) => {
   const response = await fetch("/api/group-training", {
@@ -13,6 +14,10 @@ export const useAddGroupTraining = () => {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (data: GroupTraining) => addGroupTraining(data),
-    onSuccess: () => {},
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: getAllGroupTrainingQueryKey(),
+      });
+    },
   });
 };
