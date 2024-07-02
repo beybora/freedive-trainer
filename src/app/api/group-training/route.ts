90877356fdba0ec64f9d4284448b7c7f268b9e38
borabey <bora.b.sarikaya@gmail.com>
2024-07-02 10:13:17 +0200

    add route for adding a group training and fetching all group trainings

--- src/app/api/group-training/route.ts
+++ src/app/api/group-training/route.ts
@@ -1,14 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 import connectMongoDB from "../../../../libs/mongodb";
 import GroupTraining from "../../../../models/groupTraining";
+import socket from "../services/socket";
 
 export async function POST(req: NextRequest, res: NextResponse) {
   try {
     const requestBody = await req.json();
     const groupTraining = requestBody;
+    const userId = req.headers.get("userId");
 
-      console.log("groupTraining", groupTraining);
-      
+    console.log("userId", userId);
+
     if (!groupTraining) {
       return NextResponse.json(
         { message: "Group Training data is missing in the request" },
@@ -16,11 +18,21 @@ export async function POST(req: NextRequest, res: NextResponse) {
       );
     }
 
+    if (!userId) {
+      return NextResponse.json(
+        { message: "User ID is missing in the request" },
+        { status: 400 }
+      );
+    }
+
     await connectMongoDB();
 
-    const createdGroupTraining = await GroupTraining.create({ ...groupTraining });
-      
-      console.log("createdGroupTraining", createdGroupTraining);
+    const newGroupTraining = {
+      ...groupTraining,
+      participants: [userId],
+    };
+
+    const createdGroupTraining = await GroupTraining.create(newGroupTraining);
 
     if (!createdGroupTraining) {
       return NextResponse.json(
@@ -29,10 +41,33 @@ export async function POST(req: NextRequest, res: NextResponse) {
       );
     }
 
-    return NextResponse.json({ message: "Group Training created" }, { status: 201 });
+    socket.emit("realtime-update", () => {
+      console.log("realtime-update");
+  
+    })
+
+    return NextResponse.json(
+      { message: "Group Training created" },
+      { status: 201 }
+    );
   } catch (err) {
     return NextResponse.json(
       { message: "Failed to create Group Trainign" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(req: NextRequest, res: NextResponse) {
+  try {
+    await connectMongoDB();
+
+    const groupTrainings = await GroupTraining.find();
+
+    return NextResponse.json(groupTrainings, { status: 200 });
+  } catch (err) {
+    return NextResponse.json(
+      { message: "Failed to fetch Group Trainings" },
       { status: 500 }
     );
   }
