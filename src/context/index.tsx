367ad5ab28e6f7503275c7dfadc81aed365d64d1
borabey <bora.b.sarikaya@gmail.com>
2024-07-02 09:02:03 +0200

    add new state to context provider

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -5,6 +5,7 @@ import { createContext, useContext, useState } from "react";
 export const AppContext = createContext<any>(undefined);
 
 type DiveIdType = string | undefined;
+type GroupTrainingIdType = string | undefined;
 
 type AppContextType = {
   diveId: DiveIdType;
@@ -13,8 +14,10 @@ type AppContextType = {
 
 export function AppWrapper({ children }: { children: React.ReactNode }) {
   const [diveId, setDiveId] = useState<DiveIdType>(undefined);
+  const [groupTraining, setGroupTraining] =
+    useState<GroupTrainingIdType>(undefined);
   return (
-    <AppContext.Provider value={{ diveId, setDiveId }}>
+    <AppContext.Provider value={{ diveId, setDiveId , groupTraining, setGroupTraining}}>
       {children}
     </AppContext.Provider>
   );
